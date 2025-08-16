4ca1159575bee17bcaeb9f8bc7df223d92e9bb17
Davenir Ramos <davenirramos@gmail.com>
2025-08-16 18:23:56 -0300

    Desenvolvimento, funções(adiconarAmigo, limparCampo e atualizaListaAmigos)

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
+//  Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let amigosSorteados = '';
+
+function adicionarAmigo(){
+    let nome = document.querySelector('input').value.trim(); //trim() remove espaços
+    
+    if (nome){
+        amigos.push(nome);
+        limparCampo();
+        atualizaListaAmigos();
+    } else {
+        alert('Por favor, insire um nome');
+        return;
+    }
+}
+
+function limparCampo(){
+    let nome = document.querySelector('input');
+    nome.value = '';
+}
+
+function atualizaListaAmigos(){
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++){
+        let lista = document.createElement('li');
+        lista.textContent = amigos[i];
+        listaAmigos.appendChild(lista);
+    }
+}
